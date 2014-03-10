e6b10a0bd2e63a985cc00ef895898b51520ea4ee
Daniel Morris <danieljohnmorris@gmail.com>
2014-03-10 23:38:45 +0000

    js...

--- load_films.js
+++ load_films.js
@@ -1,36 +1,64 @@
 var lastId;
 var filmCount = 0;
+var items = [];
+
+function shuffle(array) {
+  var counter = array.length, temp, index;
+
+  // While there are elements in the array
+  while (counter > 0) {
+      // Pick a random index
+      index = Math.floor(Math.random() * counter);
+
+      // Decrease counter by 1
+      counter--;
+
+      // And swap the last element with it
+      temp = array[counter];
+      array[counter] = array[index];
+      array[index] = temp;
+  }
+
+  return array;
+}
 
 function load() {
-    var json_url = "http://www.reddit.com/r/FullMovieGifs/.json";
-    if (lastId) {
-        json_url = json_url + "?after=t3_" + lastId;
-    }
-    $.getJSON(json_url, function (data) {
-        var children = data.data.children;
-        $.each(children, function (i, item) {
-            if (filmCount < maxFilmCount) {
-                filmCount = filmCount + 1;
-                $("<img/>").attr("src", item.data.url).appendTo("#images");
-            }
-        });
-        if (children && children.length > 0) {
-            lastId = children[children.length - 1].data.id;
-        } else {
-            lastId = undefined;
-        }
-    });
-}
-
-$.ajaxSetup({
+  $.ajaxSetup({
     async: false
-});
+  });
 
-for (var i=0;i<5;i++) {
-    load();
+  var json_url = "http://www.reddit.com/r/FullMovieGifs/.json";
+  if (lastId) {
+      json_url = json_url + "?after=t3_" + lastId;
+  }
+
+  $.getJSON(json_url, function (data) {
+      var children = data.data.children;
+      $.each(children, function (i, item) {
+        items.push(item);
+      });
+      if (children && children.length > 0) {
+          lastId = children[children.length - 1].data.id;
+      } else {
+          lastId = undefined;
+      }
+  });
+
+  $.ajaxSetup({
+    async: true
+  });
 }
 
-$.ajaxSetup({
-    async: true
-});
+$(window).load(function(){
+  for (var i=0;i<5;i++) {
+      load();
+  }
 
+  items = shuffle(items);
+  for (var i=0;i<maxFilmCount;i++) {
+    if (items[i]) {
+        $("<img/>").attr("src", items[i].data.url).attr("alt", items[i].data.title).appendTo("#images");
+    }
+  }
+
+});//]]>  
